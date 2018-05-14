1287b0cf3b4ecee5a52bfd0d3746118e70b416c3
stone <stone@DESKTOP-KP5NR52>
2018-05-14 17:19:18 +0800

    network

---
+++ ProDemo/NetworkWpf/NetworkWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NetworkWpf
+{
+    /// <summary>
+    /// NetworkWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class NetworkWindow : Window
+    {
+        private const string NorthwindUrl = "http://services.odata.org/Northwind/Northwind.svc/Regions";
+        private const string IncorrectUrl = "http://services.odata.org/Northwind1/Northwind.svc/Regions";
+        public NetworkWindow()
+        {
+            InitializeComponent();
+        }
+        private async Task GetDataSimpleAsync()
+        {
+            #region 正确的Url
+            using (var client = new HttpClient())
+            {
+                //接受响应
+                HttpResponseMessage response = await client.GetAsync(NorthwindUrl);
+                //判断状态code
+                if (response.IsSuccessStatusCode)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append($"Response Status Code:{(int)response.StatusCode}" + $"{response.ReasonPhrase}\r\n");
+                    sb.Append(await response.Content.ReadAsStringAsync());
+                    showText.Text = sb.ToString();
+                }
+            }
+            #endregion
+            #region 错误的url和异常示例
+            //using (var client = new HttpClient())
+            //{
+            //    //接受响应
+            //    HttpResponseMessage response = await client.GetAsync(IncorrectUrl);
+
+            //    //如果StatusCode为False，则会抛出异常
+            //    response.EnsureSuccessStatusCode();
+            //    StringBuilder sb = new StringBuilder();
+            //    sb.Append($"Response Status Code:{(int)response.StatusCode}" + $"{response.ReasonPhrase}\r\n");
+            //    sb.Append(await response.Content.ReadAsStringAsync());
+            //    showText.Text = sb.ToString();
+            //}
+            #endregion
+        }
+
+        private async void HttpClientBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await GetDataSimpleAsync();
+        }
+
+        private async void GetDataWithHeadersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await GetDataWithHeaders();
+        }
+
+        private async Task GetDataWithHeaders()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    //传递标题
+                    client.DefaultRequestHeaders.Add("Accept", "application/json;odata=verbose");
+                    StringBuilder sb = new StringBuilder();
+                    //请求头
+                    sb.Append(ShowHeaders("Request Headers:", client.DefaultRequestHeaders));
+
+                    HttpResponseMessage response = await client.GetAsync(NorthwindUrl);
+                    response.EnsureSuccessStatusCode();
+                    //响应头
+                    sb.Append(ShowHeaders("Response Headers:", response.Headers));
+
+                    showText.Text = sb.ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private string ShowHeaders(string tittle, HttpHeaders headers) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(tittle+"\r\n");
+            foreach (var item in headers)
+            {
+                string value = string.Join(" ",item.Value);
+                sb.Append($"header:{item.Key} Value:{value} \r\n");
+            }
+            sb.Append("\r\n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ ProDemo/ProDemo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AsyncWpf;
+using NetworkWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Common_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == AsyncBtn)
+            {
+                AsyncWindow asyncWindow = new AsyncWindow();
+                asyncWindow.Show();
+                this.Close();
+            }
+            else if(sender == NetworkBtn)
+            {
+                NetworkWindow netWindow = new NetworkWindow();
+                netWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
