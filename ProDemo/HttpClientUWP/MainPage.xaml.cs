d63b435ac57137ad13dc5ae986a61f9e840094a6
Stone <Stone@DESKTOP-V5RCBJA>
2018-05-15 22:49:08 +0800

    network

---
+++ ProDemo/HttpClientUWP/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace HttpClientUWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OnSendRequest() {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, Url);
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    //Http版本
+                    Version = response.Version.ToString();
+                    response.EnsureSuccessStatusCode();
+                    Result = await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog(ex.Message).ShowAsync();
+            }
+        }
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        public static readonly DependencyProperty UrlProperty =
+           DependencyProperty.Register("Url", typeof(string), typeof(MainPage), new PropertyMetadata(string.Empty));
+
+        public string Result
+        {
+            get { return (string)GetValue(ResultProperty); }
+            set { SetValue(ResultProperty, value); }
+        }
+
+        public static readonly DependencyProperty ResultProperty =
+            DependencyProperty.Register("Result", typeof(string), typeof(MainPage), new PropertyMetadata(string.Empty));
+
+        public string Version
+        {
+            get { return (string)GetValue(VersionProperty); }
+            set { SetValue(VersionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Version.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VersionProperty =
+            DependencyProperty.Register("Version", typeof(string), typeof(MainPage), new PropertyMetadata(string.Empty));
+    }
+
+
+
+    
+
+
+}
