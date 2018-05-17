70ac5d3bb9184e497e6ddd4963c5bc55729f95ed
stone <stone@DESKTOP-KP5NR52>
2018-05-17 17:25:41 +0800

    Tcp

--- ProDemo/NetworkWpf/NetworkWindow.xaml.cs
+++ ProDemo/NetworkWpf/NetworkWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -75,6 +78,7 @@ namespace NetworkWpf
             #endregion
         }
         #endregion
+
         #region HttpClient标题
         /// <summary>
         /// 单击事件
@@ -135,6 +139,7 @@ namespace NetworkWpf
             return sb.ToString();
         }
         #endregion
+
         #region Uri和Uribuilder
         private void UriBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -161,8 +166,94 @@ namespace NetworkWpf
             Uri uri = builder.Uri;
             showText.Text = uri.ToString();
         }
+
         #endregion
 
+        #region IPAddress
+        private void IpAddressBtn_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress address;
+            IPAddress.TryParse("65.52.128.33", out address);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte item in address.GetAddressBytes())
+            {
+                int i = 1;
+                sb.Append($"byte{i}：{item:X}\r\n");
+            }
+            sb.Append($"AddressFamily：{address.AddressFamily}\r\n");
+            sb.Append($"MapToIPv6：{address.MapToIPv6()}\r\n");
+            sb.Append($"静态属性\r\n");
+            sb.Append($"IPv4 loopback address：{IPAddress.Loopback}\r\n");
+            sb.Append($"IPv6 loopback address：{IPAddress.IPv6Loopback}\r\n");
+            sb.Append($"IPv4 broadcast address：{IPAddress.Broadcast}\r\n");
+            sb.Append($"IPv4 any address：{IPAddress.Any}\r\n");
+            sb.Append($"IPv6 any address：{IPAddress.IPv6Any}\r\n");
+            showText.Text = sb.ToString();
+        }
+        #endregion
+
+        #region Dns
+        private async void DnsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            await OnLookupAsync();
+        }
+
+        private async Task OnLookupAsync() {
+
+            IPHostEntry ipHost = await Dns.GetHostEntryAsync("www.orf.at");
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Hostname：{ipHost.HostName}\r\n");
+
+            foreach (var item in ipHost.AddressList)
+            {
+                sb.Append($"Address Family：{item.AddressFamily}\r\n");
+                sb.Append($"Address：{item.MapToIPv4()}\r\n");
+            }
+            showText.Text = sb.ToString();
+        }
+        #endregion
+
+        private async void HttpClientUsingTCPBtn_Click(object sender, RoutedEventArgs e)
+        {
+            showText.Text = await RequestHtmlAsync("10.1.1.11");
+        }
+
+        private const int ReadBufferSize = 1024;
+        private async Task<string> RequestHtmlAsync(string hostName) {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    await client.ConnectAsync(hostName,80);
+                    NetworkStream stream = client.GetStream();
+                    string header = "GET / HTTP/1.1\r\n" +
+                        $"Host：{hostName}:80\r\n" +
+                        "Connection：close\r\n" +
+                        "\r\n";
+                    byte[] buffer = Encoding.UTF8.GetBytes(header);
+                    await stream.WriteAsync(buffer, 0, buffer.Length);
+                    await stream.FlushAsync();
+
+                    var ms = new MemoryStream();
+                    int read = 0;
+                    do
+                    {
+                        read = await stream.ReadAsync(buffer, 0, ReadBufferSize);
+                        ms.Write(buffer, 0, read);
+                        Array.Clear(buffer, 0, buffer.Length);
+                       
+                    } while (read > 0);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var reader = new StreamReader(ms);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
 
     }
 }
