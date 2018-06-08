586a302e442ea9e960b5fd5efd7f8afe4e5cfbef
stone <stone@DESKTOP-KP5NR52>
2018-06-08 17:04:07 +0800

    socket

---
+++ ProDemo/NetworkWpf/SocketSample/SocketServer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetworkWpf.SocketSample
+{
+    public class SocketServer
+    {
+        /// <summary>
+        /// 侦听器创建一个新的Socket对象。
+        /// 例如，用TCP与IPv4，地址系列就必须是InterNetwork，流套接字类型Stream、协议类型TCP。
+        /// 要使用IPv4创建一个UDP通信，地址系列就需要设置为InterNetwork、套接字类型Dgram和协议类型Udp。
+        /// </summary>
+        /// <param name="port"></param>
+        public void Listener(int port)
+        {
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.ReceiveTimeout = 5000;
+            listener.SendTimeout = 5000;
+
+            listener.Bind(new IPEndPoint(IPAddress.Any, port));
+            // 定义了服务器的缓冲区队列的大小一一在处理连接之前，可以同时连接多少客户端
+            listener.Listen(backlog: 15);
+            Console.WriteLine($"侦听器在端口{port}正在侦听");
+
+
+            var cts = new CancellationTokenSource();
+
+            var tf = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
+            tf.StartNew(() =>
+            {
+                Console.WriteLine("侦听任务开始");
+                while (true)
+                {
+                    if (cts.Token.IsCancellationRequested)
+                    {
+                        cts.Token.ThrowIfCancellationRequested();
+                        break;
+                    }
+                    Console.WriteLine("等待接受");
+                    // 等待客户端连接在Socket类的方法Accept中进行。这个方法阻塞线程，直到客户机连接为止。
+                    // 客户端连接后，需要再次调用这个方法，来满足其他客户端的请求；所以在while循环中调用此方法。
+                    Socket client = listener.Accept();
+                    if (!client.Connected)
+                    {
+                        Console.WriteLine("没有连接上");
+                        continue;
+                    }
+
+                    Console.WriteLine($"客户端已连接当前地址 {((IPEndPoint)client.LocalEndPoint).Address} 端口号 {((IPEndPoint)client.LocalEndPoint).Port}" +
+                        $"路由地址{((IPEndPoint)client.RemoteEndPoint).Address} 路由端口{((IPEndPoint)client.RemoteEndPoint).Port}");
+                    // 为了进行侦听，启动一个单独的任务，该任务可以在调用线程中取消。在方法CommunicateWithClientUsingSocketAsync中执行使用套接字读写的任务。
+                    // 这个方法接收绑定到客户端的Socket实例，进行读写：
+                    Task t = CommunicateWithClientUsingSocketAsync(client);
+                }
+                listener.Dispose();
+                Console.WriteLine("侦听任务关闭");
+            }, cts.Token);
+
+            Console.WriteLine("请按任意键退出");
+            Console.ReadLine();
+            cts.Cancel();
+        }
+        /// <summary>
+        /// 为了与客户端沟通，创建一个新任务。
+        /// 这会释放侦听器任务，立即进行下一次迭代，等待下一个客户端连接。
+        /// Socket类的Receive方法接受一个缓冲，其中的数据和标志可以读取，用于套接字。
+        /// 这个字节数组转换为字符串，使用Send方法，连同一个小变化一起发送回客户机
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <returns></returns>
+        private Task CommunicateWithClientUsingSocketAsync(Socket socket)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    using (socket)
+                    {
+                        bool completed = false;
+                        do
+                        {
+                            byte[] readbuffer = new byte[1024];
+                            int read = socket.Receive(readbuffer, 0, readbuffer.Length, SocketFlags.None);
+                            string fromClient = Encoding.UTF8.GetString(readbuffer, 0, read);
+                            Console.WriteLine($"read:{read} bytes:{fromClient}");
+                            if (string.Compare(fromClient, "shutdown", ignoreCase: true) == 0)
+                            {
+                                completed = true;
+                            }
+
+                            byte[] writebuffer = Encoding.UTF8.GetBytes($"echo{fromClient}");
+
+                            int send = socket.Send(writebuffer);
+                            Console.WriteLine($"send:{send} bytes");
+                        } while (!completed);
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        /// <summary>
+        /// 使用NetWorkStream和套接字
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <returns></returns>
+        private async Task CommunicateWithClientUsingReadersAndWritersAsync(Socket socket)
+        {
+            IList<string> list = new List<string>();
+            var list2 =  list.Where(l => l == "123").ToList();
+            try
+            {
+                using (var stream = new NetworkStream(socket, ownsSocket: true))
+                {
+                    bool completed = true;
+                    do
+                    {
+                        byte[] readbuffer = new byte[1024];
+                        int read = await stream.ReadAsync(readbuffer, 0, 1024);
+                        string fromClient = Encoding.UTF8.GetString(readbuffer, 0, read);
+                        Console.WriteLine($"read{read}bytes:{fromClient}");
+                        if (string.Compare(fromClient, "shutdown", ignoreCase: true) == 0)
+                        {
+                            completed = true;
+                        }
+                        byte[] writeBuffer = Encoding.UTF8.GetBytes($"echo{fromClient}");
+
+                        await stream.WriteAsync(writeBuffer, 0, writeBuffer.Length);
+                    } while (!completed);
+                }
+                Console.WriteLine($"关闭流和socket客户端");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
