68abf91648d878fa6b8eea90d64af7eaac55fa60
Stone <Stone@DESKTOP-V5RCBJA>
2018-06-13 23:56:53 +0800

    lock

--- ProDemo/SynchronizationWpf/MainWindow.xaml.cs
+++ ProDemo/SynchronizationWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SynchronizationWpf.SynchronizationSample;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace SynchronizationWpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DealLockSampleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ShowTb.Text += new SynchronizationSampleMain().DealLockSample();
         }
     }
 }
