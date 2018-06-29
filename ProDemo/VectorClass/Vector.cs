168273c20784e60e53706d8408fb0d0f06054e73
Dream <Dream@DESKTOP-I1SSGPI>
2018-06-29 16:50:27 +0800

    r&d

---
+++ ProDemo/VectorClass/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using WhatsNewAttributes;
+
+namespace VectorClass
+{
+    [LastModified("2015年6月10日", "updated for C# 6 and .NET Core")]
+    [LastModified("2010年12月14日", "IEnumerable interface implemented:" + "Vector可以作为一个集合来对待")]
+    [LastModified("2010年2月10日", "IFormattable interface implemented" + "Vector接受N和VE格式的说明符")]
+    public class Vector : IFormattable, IEnumerable<double>
+    {
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector(Vector vector) : this(vector.X, vector.Y, vector.Z)
+        {
+
+        }
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+
+        
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerator<double> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProDemo/WhatsNewAttributes/WhatsNewAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace WhatsNewAttributes
+{
+    /// <summary>
+    /// 在现实生活中，或许想把特性应用到任何对象上。
+    /// 为了使代码比较简单，这里仅允许将它应用于类和方法 ，
+    /// 并允许它多次应用到同一项上(AllowMultiple = true),
+    /// 因为可以多次修改某一项，每次修改都需要用一个不同的忒行实例来标记。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class LastModifiedAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        ///
+        /// </summary>
+        private readonly DateTime _dateModified;
+
+        private readonly string _changes;
+
+        public LastModifiedAttribute(string dateModified, string changes)
+        {
+            _dateModified = DateTime.Parse(dateModified);
+            _changes = changes;
+        }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime DateModified => _dateModified;
+        /// <summary>
+        /// 描述修改信息
+        /// </summary>
+        public string Changes => _changes;
+        /// <summary>
+        /// 描述该数据项的任何重要问题
+        /// </summary>
+        public string Issues { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class SupportsWhatsNewAttribute : Attribute
+    {
+
+    }
+}
