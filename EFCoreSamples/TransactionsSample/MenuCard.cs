f7a3984e8a64c7646d80be5bbc359324ba759afe
Dream <Dream@DESKTOP-I1SSGPI>
2018-09-03 17:50:23 +0800

    transaction

---
+++ EFCoreSamples/TransactionsSample/MenuCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransactionsSample
+{
+    //[Table("MenuCards", Schema = "mc")]
+    public class MenuCard
+    {
+        public int MenuCardId { get; set; }
+        //[MaxLength(120)]
+        public string Title { get; set; }
+        public List<Menu> Menus { get; } = new List<Menu>();
+        public override string ToString() => Title;
+    }
+}
